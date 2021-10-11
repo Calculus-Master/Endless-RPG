54d19982e4900e7cf40bc88238ff0ad6324916df
Saptarshi Mallick <sapmallick@gmail.com>
2021-10-11 10:40:17 -0700

    Added Battle factory methods, BattleType (PVP/PVE), and Battle#createPlayers

--- src/main/java/com/calculusmaster/endlessrpg/gameplay/battle/Battle.java
+++ src/main/java/com/calculusmaster/endlessrpg/gameplay/battle/Battle.java
@@ -1,13 +1,70 @@
 package com.calculusmaster.endlessrpg.gameplay.battle;
 
+import com.calculusmaster.endlessrpg.gameplay.battle.player.AIPlayer;
+import com.calculusmaster.endlessrpg.gameplay.battle.player.AbstractPlayer;
+import com.calculusmaster.endlessrpg.gameplay.battle.player.UserPlayer;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Battle
 {
-    public static Battle create(String p1ID, String p2ID)
+    private BattleType battleType;
+    private List<AbstractPlayer> players;
+
+    //Creators
+
+    public static Battle createPVP(String p1ID, String p2ID)
     {
         Battle b = new Battle();
 
-
+        b.setBattleType(BattleType.PVP);
+        b.createPlayers(p1ID, p2ID);
 
         return b;
+    }
+
+    public static Battle createPVE(String userID)
+    {
+        Battle b = new Battle();
+
+        b.setBattleType(BattleType.PVE);
+        b.createPlayers(userID);
+
+        return b;
+    }
+
+    //Battle Setup - Common
+    private void setBattleType(BattleType type)
+    {
+        this.battleType = type;
+    }
+
+    //Battle Setup - PvP
+    private void createPlayers(String p1ID, String p2ID)
+    {
+        this.players = new ArrayList<>();
+        this.players.add(new UserPlayer(p1ID));
+        this.players.add(new UserPlayer(p2ID));
+    }
+
+    //Battle Setup - PvE
+    private void createPlayers(String userID)
+    {
+        this.players = new ArrayList<>();
+        this.players.add(new UserPlayer(userID));
+        this.players.add(new AIPlayer());
+    }
+
+    //Accessors
+    public BattleType getBattleType()
+    {
+        return this.battleType;
+    }
+
+    private enum BattleType
+    {
+        PVP,
+        PVE;
     }
 }
