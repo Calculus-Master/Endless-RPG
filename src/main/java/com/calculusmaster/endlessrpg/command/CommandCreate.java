39d1ee8bcec303327cde641eb5ff81ecd12a4b65
Saptarshi Mallick <sapmallick@gmail.com>
2021-10-12 21:46:32 -0700

    Creating new characters now requires increasing amounts of gold

--- src/main/java/com/calculusmaster/endlessrpg/command/CommandCreate.java
+++ src/main/java/com/calculusmaster/endlessrpg/command/CommandCreate.java
@@ -20,18 +20,36 @@ public class CommandCreate extends Command
         if(this.msg.length == 1) this.response = "You need to include the character name!";
         else
         {
-            RPGCharacter c = RPGCharacter.create(this.rawMultiWordContent(1));
-
             if(!registered)
             {
                 PlayerDataQuery.register(this.player.getId(), this.player.getName());
                 this.playerData = new PlayerDataQuery(this.player.getId());
             }
 
+            String gold = "";
+            if(!this.playerData.getCharacterList().isEmpty())
+            {
+                int cost = (int)Math.pow(100 * this.playerData.getCharacterList().size(), 1.31);
+                cost -= cost % 100;
+
+                if(this.playerData.getGold() < cost)
+                {
+                    this.response = "You don't have enough Gold to create a new Character! You need `" + cost + "` Gold!";
+                    return this;
+                }
+                else
+                {
+                    this.playerData.removeGold(cost);
+                    gold = " (Cost: " + cost + " Gold)";
+                }
+            }
+
+            RPGCharacter c = RPGCharacter.create(this.rawMultiWordContent(1));
+
             c.upload();
             this.playerData.addCharacter(c.getCharacterID());
 
-            this.response = "`" + c.getName() + "` has joined the world!";
+            this.response = "`" + c.getName() + "` has joined the world!" + gold;
         }
         return this;
     }
