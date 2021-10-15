48a8f6d262dcff764e726afc351bd8d3aa085df1
Saptarshi Mallick <sapmallick@gmail.com>
2021-10-15 13:07:54 -0700

    Added 3 more Developer commands, updated some older ones

--- src/main/java/com/calculusmaster/endlessrpg/command/misc/CommandDeveloper.java
+++ src/main/java/com/calculusmaster/endlessrpg/command/misc/CommandDeveloper.java
@@ -5,11 +5,16 @@ import com.calculusmaster.endlessrpg.gameplay.battle.Battle;
 import com.calculusmaster.endlessrpg.gameplay.character.RPGCharacter;
 import com.calculusmaster.endlessrpg.gameplay.enums.LootType;
 import com.calculusmaster.endlessrpg.gameplay.enums.RPGClass;
+import com.calculusmaster.endlessrpg.gameplay.enums.Stat;
 import com.calculusmaster.endlessrpg.gameplay.loot.LootBuilder;
 import com.calculusmaster.endlessrpg.gameplay.loot.LootItem;
+import com.calculusmaster.endlessrpg.mongo.PlayerDataQuery;
 import com.calculusmaster.endlessrpg.util.Mongo;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.ArrayList;
 
 public class CommandDeveloper extends Command
 {
@@ -27,23 +32,20 @@ public class CommandDeveloper extends Command
         {
             switch(this.msg[1])
             {
-                case "resetself" -> {
-                    this.playerData.getCharacterList().stream().map(RPGCharacter::build).forEach(RPGCharacter::delete);
-                    Mongo.PlayerData.deleteOne(Filters.eq(this.player.getId()));
-                }
-                case "forceclasschange" -> {
+                case "setclass" -> {
+                    PlayerDataQuery target = this.getMentions().size() > 0 ? new PlayerDataQuery(this.getMentions().get(0).getId()) : this.playerData;
                     RPGClass clazz = RPGClass.cast(this.msg[2]);
-                    RPGCharacter c = this.playerData.getActiveCharacter();
+                    RPGCharacter c = target.getActiveCharacter();
 
                     c.setRPGClass(clazz);
                     c.updateRPGClass();
                 }
-                case "clearlootdb" -> Mongo.LootData.deleteMany(Filters.exists("lootID"));
                 case "addloot" -> {
-                    int activeLevel = this.playerData.getActiveCharacter().getLevel();
+                    PlayerDataQuery target = this.getMentions().size() > 0 ? new PlayerDataQuery(this.getMentions().get(0).getId()) : this.playerData;
+                    int activeLevel = target.getActiveCharacter().getLevel();
                     LootItem loot = LootBuilder.reward(LootType.cast(this.msg[2]), activeLevel + 1);
                     loot.upload();
-                    this.playerData.addLootItem(loot.getLootID());
+                    target.addLootItem(loot.getLootID());
                 }
                 case "clearbattles" -> Battle.BATTLES.clear();
                 case "reset" -> {
@@ -51,10 +53,30 @@ public class CommandDeveloper extends Command
                     Mongo.CharacterData.deleteMany(Filters.exists("characterID"));
                     Mongo.PlayerData.deleteMany(Filters.exists("playerID"));
                 }
-                case "forcelevel" -> {
+                case "setlevel" -> {
+                    PlayerDataQuery player = this.getMentions().size() > 0 ? new PlayerDataQuery(this.getMentions().get(0).getId()) : this.playerData;
                     int target = this.getInt(2);
-                    RPGCharacter active = this.playerData.getActiveCharacter();
+
+                    RPGCharacter active = player.getActiveCharacter();
                     while(active.getLevel() != target) active.addExp(active.getExpRequired(active.getLevel() + 1));
+                    active.completeUpdate();
+                }
+                case "addgold" -> {
+                    int amount = this.getInt(2);
+                    if(this.getMentions().size() > 0) new PlayerDataQuery(this.getMentions().get(0).getId()).addGold(amount);
+                    else this.playerData.addGold(amount);
+                }
+                case "deleteloot" -> {
+                    Mongo.LootData.deleteMany(Filters.exists("lootID"));
+                    Mongo.PlayerData.updateMany(Filters.exists("playerID"), Updates.set("loot", new ArrayList<>()));
+                }
+                case "addcorestat" -> {
+                    PlayerDataQuery target = this.getMentions().size() > 0 ? new PlayerDataQuery(this.getMentions().get(0).getId()) : this.playerData;
+                    Stat s = Stat.cast(this.msg[2]);
+                    int amount = this.getInt(3);
+
+                    RPGCharacter active = target.getActiveCharacter();
+                    active.increaseCoreStat(s, amount);
                     active.completeUpdate();
                 }
                 default -> throw new IllegalStateException("Invalid Developer Command. Input: " + this.msg[0]);
