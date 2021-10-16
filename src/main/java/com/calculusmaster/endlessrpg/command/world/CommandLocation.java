b9016f0e59071c3971a1f8cb24addb7d77e547c5
Saptarshi Mallick <sapmallick@gmail.com>
2021-10-16 07:47:33 -0700

    Added LocationType information to r!location

--- src/main/java/com/calculusmaster/endlessrpg/command/world/CommandLocation.java
+++ src/main/java/com/calculusmaster/endlessrpg/command/world/CommandLocation.java
@@ -24,9 +24,9 @@ public class CommandLocation extends Command
 
         this.embed
                 .setTitle(Realm.CURRENT.getName())
-                .addField("Current Location", "**" + location.getName() + "**", true)
-                .addField("Current Time", "**" + Global.normalize(location.getTime() + "**"), true)
-                .addField("Current Weather", "**" + Global.normalize(location.getWeather() + "**"), true)
+                .addField("Current Location", "**" + location.getName() + "**\nType: " + Global.normalize(location.getType().toString().replaceAll("_", " ")), true)
+                .addField("Current Time", "**" + Global.normalize(location.getTime().toString()) + "**", true)
+                .addField("Current Weather", "**" + Global.normalize(location.getWeather().toString()) + "**", true)
                 .addField(this.getVisitedOverview());
 
         //TODO: Detailed description of Location (with Type), detailed description of weather effects, detailed description of realm effects
