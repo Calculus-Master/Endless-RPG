01de3d4825093b5f7b40219e0eeddcaf65b6719d
Saptarshi Mallick <sapmallick@gmail.com>
2021-10-19 22:22:07 -0700

    Dungeon now uses the Location level attribute (modifier of the player level)

--- src/main/java/com/calculusmaster/endlessrpg/command/activity/CommandDungeon.java
+++ src/main/java/com/calculusmaster/endlessrpg/command/activity/CommandDungeon.java
@@ -3,7 +3,6 @@ package com.calculusmaster.endlessrpg.command.activity;
 import com.calculusmaster.endlessrpg.command.core.Command;
 import com.calculusmaster.endlessrpg.gameplay.battle.Battle;
 import com.calculusmaster.endlessrpg.gameplay.battle.Dungeon;
-import com.calculusmaster.endlessrpg.gameplay.character.RPGCharacter;
 import com.calculusmaster.endlessrpg.gameplay.enums.LocationType;
 import com.calculusmaster.endlessrpg.gameplay.world.Realm;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -33,11 +32,11 @@ public class CommandDungeon extends Command
             else
             {
                 //TODO: Replace with Location level attribute
-                int averageLevel = this.playerData.getCharacterList().stream().map(RPGCharacter::build).mapToInt(RPGCharacter::getLevel).sum() / this.playerData.getCharacterList().size();
+                int dungeonLevel = this.playerData.getLevel() + Realm.CURRENT.getLocation(this.playerData.getLocationID()).getLevel();
 
                 Dungeon d = current.equals(LocationType.FINAL_KINGDOM)
-                        ? Dungeon.createFinalKingdom(this.playerData, Realm.CURRENT.getLocation(this.playerData.getLocationID()), averageLevel, this.event)
-                        : Dungeon.create(this.playerData, Realm.CURRENT.getLocation(this.playerData.getLocationID()), averageLevel, this.event);
+                        ? Dungeon.createFinalKingdom(this.playerData, Realm.CURRENT.getLocation(this.playerData.getLocationID()), dungeonLevel, this.event)
+                        : Dungeon.create(this.playerData, Realm.CURRENT.getLocation(this.playerData.getLocationID()), dungeonLevel, this.event);
 
                 d.sendStartEmbed();
 
