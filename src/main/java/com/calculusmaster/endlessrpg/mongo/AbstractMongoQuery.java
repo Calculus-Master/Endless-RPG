3b87bc3b36d43d08aa9ef431889871d7d61b4658
Saptarshi Mallick <sapmallick@gmail.com>
2021-12-19 12:42:37 -0700

    Added logging for every Database Update that happens

--- src/main/java/com/calculusmaster/endlessrpg/mongo/AbstractMongoQuery.java
+++ src/main/java/com/calculusmaster/endlessrpg/mongo/AbstractMongoQuery.java
@@ -1,11 +1,13 @@
 package com.calculusmaster.endlessrpg.mongo;
 
+import com.calculusmaster.endlessrpg.util.helpers.LoggerHelper;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public abstract class AbstractMongoQuery
 {
@@ -29,12 +31,16 @@ public abstract class AbstractMongoQuery
     {
         this.database.updateOne(this.query, update);
 
+        LoggerHelper.warn(this.getClass(), "Database Update: " + this.database.getNamespace().getCollectionName() + ", Update Query: " + update);
+
         this.update();
     }
 
     protected void update(Bson... updates)
     {
         this.database.updateOne(this.query, Arrays.asList(updates));
+
+        LoggerHelper.warn(this.getClass(), "Database Update: " + this.database.getNamespace().getCollectionName() + ", Update Query: " + Arrays.stream(updates).map(Object::toString).collect(Collectors.joining(", ")));
 
         this.update();
     }
