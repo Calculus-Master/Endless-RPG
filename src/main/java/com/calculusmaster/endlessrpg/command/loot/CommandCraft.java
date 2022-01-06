a953cab95eb73e5bdd2b210e6a0c492a7183aa93
Calculus-Master <sapmallick@gmail.com>
2022-01-05 18:29:26 -0700

    Bank Access Update Part 3: r!craft now pulls all costs from active character instead of bank

--- src/main/java/com/calculusmaster/endlessrpg/command/loot/CommandCraft.java
+++ src/main/java/com/calculusmaster/endlessrpg/command/loot/CommandCraft.java
@@ -3,12 +3,12 @@ package com.calculusmaster.endlessrpg.command.loot;
 import com.calculusmaster.endlessrpg.command.core.Command;
 import com.calculusmaster.endlessrpg.command.core.CommandInvalid;
 import com.calculusmaster.endlessrpg.gameplay.character.RPGCharacter;
-import com.calculusmaster.endlessrpg.gameplay.character.RPGResourceContainer;
 import com.calculusmaster.endlessrpg.gameplay.enums.LootComponentType;
 import com.calculusmaster.endlessrpg.gameplay.loot.LootComponent;
 import com.calculusmaster.endlessrpg.gameplay.resources.enums.Resource;
-import com.calculusmaster.endlessrpg.gameplay.world.Realm;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.concurrent.Executors;
 
 public class CommandCraft extends Command
 {
@@ -30,34 +30,23 @@ public class CommandCraft extends Command
         if(component == null) return this.invalid("Invalid Component Type!");
         else if(resource == null) return this.invalid("Invalid Resource Name!");
 
-        boolean town = Realm.CURRENT.getLocation(this.playerData.getLocationID()).getType().isTown();
         RPGCharacter active = this.playerData.getActiveCharacter();
 
-        RPGResourceContainer source = town ? this.playerData.getResources() : active.getResources();
-        int goldSource = town ? this.playerData.getGold() : active.getGold();
-
-        if(!source.has(resource) || source.get(resource) < component.getMaterialAmount()) return this.invalid("You do not have enough " + resource.getName() + " to craft a " + resource.getName() + " " + component.getName() + "!");
-        else if(goldSource < component.getGoldCost()) return this.invalid("You do not have enough gold to craft a " + resource.getName() + " " + component.getName() + "!");
+        if(!active.getResources().has(resource) || active.getResources().get(resource) < component.getMaterialAmount()) return this.invalid("You do not have enough " + resource.getName() + " to craft a " + resource.getName() + " " + component.getName() + "!");
+        else if(active.getGold() < component.getGoldCost()) return this.invalid("You do not have enough gold to craft a " + resource.getName() + " " + component.getName() + "!");
         else
         {
-            if(town) this.playerData.removeGold(component.getGoldCost());
-            else
-            {
-                active.removeGold(component.getGoldCost());
-                active.updateGold();
-            }
-
-            if(town) this.playerData.removeResource(resource, component.getMaterialAmount());
-            else
-            {
-                active.getResources().decrease(resource, component.getMaterialAmount());
-                active.updateResources();
-            }
+            active.removeGold(component.getGoldCost());
+            active.getResources().decrease(resource, component.getMaterialAmount());
 
             LootComponent crafted = LootComponent.create(component, resource);
-
-            crafted.upload();
             this.playerData.addComponent(crafted.getComponentID());
+
+            Executors.newSingleThreadScheduledExecutor().execute(() -> {
+                active.updateGold();
+                active.updateResources();
+                crafted.upload();
+            });
 
             this.response = "You successfully crafted a " + crafted.getName() + "!\n\nThis component used " + component.getMaterialAmount() + " " + resource.getName() + ", and cost " + component.getGoldCost() + " Gold.";
         }
